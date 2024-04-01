80bc05d8d5ea957caf04194bb767900df061541a
Ruben Movsisyan <rubenmovsisyan@Rubens-MacBook-Pro-2.local>
2024-03-31 23:48:05 -0700

    added widget support for app

---
+++ Dex3Widget/Dex3WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Dex3WidgetBundle.swift
+//  Dex3Widget
+//
+//  Created by Ruben Movsisyan on 3/31/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Dex3WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Dex3Widget()
+    }
+}
